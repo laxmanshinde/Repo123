52fd3c1b8ac9bc93d2b104842387d7313129496c
laxmanshinde <103309137+laxmanshinde@users.noreply.github.com>
2022-06-20 12:17:36 +0530

    Update login.java

--- repo123/src/main/java/pageLayer/login.java
+++ repo123/src/main/java/pageLayer/login.java
@@ -6,6 +6,9 @@ public class login {
 	public static void login()
 	{
 		System.out.println("login");
+		System.out.println("login1");
+		System.out.println("login2");
+		System.out.println("login3");
 	}
 
 }
